eead433a713c38897e5b80b9817f3b3681ed4004
Erick Garcia <erickarmandogarcia@hotmail.com>
2014-12-03 12:14:31 -0500

    GD resizer for php

---
+++ php/resizerImage.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * PHP function to resize an image maintaining aspect ratio
+ * http://salman-w.blogspot.com/2008/10/resize-images-using-phpgd-library.html
+ *
+ * Creates a resized (e.g. thumbnail, small, medium, large)
+ * version of an image file and saves it as another file
+ */
+//Set the new width and height
+define('THUMBNAIL_IMAGE_MAX_WIDTH', 400);
+define('THUMBNAIL_IMAGE_MAX_HEIGHT', 400);
+
+function generate_image_thumbnail($source_image_path, $thumbnail_image_path)
+{
+    list($source_image_width, $source_image_height, $source_image_type) = getimagesize($source_image_path);
+    switch ($source_image_type) {
+        case IMAGETYPE_GIF:
+            $source_gd_image = imagecreatefromgif($source_image_path);
+            break;
+        case IMAGETYPE_JPEG:
+            $source_gd_image = imagecreatefromjpeg($source_image_path);
+            break;
+        case IMAGETYPE_PNG:
+            $source_gd_image = imagecreatefrompng($source_image_path);
+            break;
+    }
+    if ($source_gd_image === false) {
+        return false;
+    }
+    $source_aspect_ratio = $source_image_width / $source_image_height;
+    $thumbnail_aspect_ratio = THUMBNAIL_IMAGE_MAX_WIDTH / THUMBNAIL_IMAGE_MAX_HEIGHT;
+    if ($source_image_width <= THUMBNAIL_IMAGE_MAX_WIDTH && $source_image_height <= THUMBNAIL_IMAGE_MAX_HEIGHT) {
+        $thumbnail_image_width = $source_image_width;
+        $thumbnail_image_height = $source_image_height;
+    } elseif ($thumbnail_aspect_ratio > $source_aspect_ratio) {
+        $thumbnail_image_width = (int) (THUMBNAIL_IMAGE_MAX_HEIGHT * $source_aspect_ratio);
+        $thumbnail_image_height = THUMBNAIL_IMAGE_MAX_HEIGHT;
+    } else {
+        $thumbnail_image_width = THUMBNAIL_IMAGE_MAX_WIDTH;
+        $thumbnail_image_height = (int) (THUMBNAIL_IMAGE_MAX_WIDTH / $source_aspect_ratio);
+    }
+    $thumbnail_gd_image = imagecreatetruecolor($thumbnail_image_width, $thumbnail_image_height);
+    imagecopyresampled($thumbnail_gd_image, $source_gd_image, 0, 0, 0, 0, $thumbnail_image_width, $thumbnail_image_height, $source_image_width, $source_image_height);
+    imagejpeg($thumbnail_gd_image, $thumbnail_image_path, 90);
+    imagedestroy($source_gd_image);
+    imagedestroy($thumbnail_gd_image);
+    return true;
+}
+
+generate_image_thumbnail("C:/xampp/htdocs/test-Resizer/hermosas.jpeg","C:/xampp/htdocs/test-Resizer/hermosas2.jpeg")
+?>
