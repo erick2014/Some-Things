bc4b941fe5ad0f1940150c831b216457498c26cc
erick <erickarmandogarcia@hotmail.com>
2015-01-20 14:14:42 -0500

    Fix upload validations into upload.php

--- js/uploadJquery/upload.php
+++ js/uploadJquery/upload.php
@@ -8,22 +8,31 @@ Released under the MIT License <http://www.opensource.org/licenses/mit-license.p
 // Define a destination
 $targetFolder = '/uploads'; // Relative to the root
 
-if ( !empty($_FILES) && isset($_FILES["file"]) && $_FILES["file"]["size"]<=57000 ) {
-	$tempFile = $_FILES['file']['tmp_name'];
-	$targetPath = $_SERVER['DOCUMENT_ROOT']."/dropzone/" . $targetFolder;
-	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['file']['name'];
+if ( !empty($_FILES) && isset($_FILES["file"]) ) {
+	//validate the image size
+	if( $_FILES['file']['size'] <17000){
+		$tempFile = $_FILES['file']['tmp_name'];
+		$targetPath = $_SERVER['DOCUMENT_ROOT']."/dropzone/" . $targetFolder;
+		$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['file']['name'];
 
-	// Validate the file type
-	$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
-	$fileParts = pathinfo($_FILES['file']['name']);
+		// Validate the file type
+		$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
+		$fileParts = pathinfo($_FILES['file']['name']);
 
-	if (in_array($fileParts['extension'],$fileTypes)) {
-		move_uploaded_file($tempFile,$targetFile);
-		echo 'imagen subida correctamente!'.$_FILES['file']['size'];
+		if (in_array($fileParts['extension'],$fileTypes)) {
+			move_uploaded_file($tempFile,$targetFile);
+			echo 'imagen subida correctamente!';
+		}
+		else {
+			echo 'Tipo de imagen no valido';
+		}
 	}
-	else {
-		echo 'Tipo de imagen no valido"';
+	else{
+		echo "la imagen es muy pesada";
 	}
+}
+else{
+	echo "Image wasn't received";
 }
 
 ?>
